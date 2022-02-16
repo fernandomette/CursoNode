482dfad465950c50eaada9e5377dd82651a5a08d
Fernando Mette <metteinfor@gmail.com>
2022-02-16 18:15:33 -0300

    criando conexao com o banco

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,18 @@
 module.exports = function (app) {
     app.get('/noticias', function(req, res){
-        res.render("noticias/noticias");
+
+        let mysql = require('mysql');
+
+        let connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : 'jehh8494',
+            database : 'porta_noticias'
+        });
+        connection.query('select * from noticia', function(erro, result){
+            res.send(result);
+        });
+
+        //res.render("noticias/noticias");
     });
 };
--- app/routes/tecnologia.js
+++ app/routes/tecnologia.js
@@ -1,5 +1,18 @@
 module.exports = function (app) {
     app.get('/tecnologia', function(req, res){
-        res.render("secao/tecnologia");
+        //res.render("secao/tecnologia");
+        let mysql = require('mysql');
+
+        let connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : 'jehh8494',
+            database : 'porta_noticias'
+        });
+        connection.query('select * from noticia', function(erro, result){
+            res.send(result);
+            console.log(result);
+        });
+
     });
 };
