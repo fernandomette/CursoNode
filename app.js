4e0a9b2c80ffa8117e436884b90d4a509ae578cf
Fernando Mette <metteinfor@gmail.com>
2022-02-16 17:33:11 -0300

    Baixando modulo mysql e organizando estrutura

---
+++ app.js
@@ -0,0 +1,30 @@
+/**
+Instalar pacote express
+npm install express --save
+
+Instalar pacote ejs
+npm install ejs --save
+  
+npm install mysql --save
+
+Instalar nodemon
+npm install -g nodemon
+Compilar utilizando nodemon index.js .... 
+
+*/
+
+let app = require('./config/server');
+
+let formlaster = require('./app/routes/formulario_inclusao_noticias')(app);
+
+let routehome = require('./app/routes/home')(app);
+
+let moda = require('./app/routes/moda')(app);
+
+let noticias = require('./app/routes/noticias')(app);
+
+let tecnologia = require('./app/routes/tecnologia')(app);
+
+app.listen(3000, function(){
+    console.log("Rodando express");
+})
