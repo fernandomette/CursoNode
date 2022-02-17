5f4540010156d336ed7ae3a310e886cbd3b7278c
Fernando Mette <fernando@fastbuilt.com.br>
2022-02-17 11:59:10 -0300

    alterando tecnologia

--- app/routes/tecnologia.js
+++ app/routes/tecnologia.js
@@ -1,18 +1,6 @@
 module.exports = function (app) {
     app.get('/tecnologia', function (req, res) {
-        //res.render("secao/tecnologia");
-        let mysql = require('mysql');
-
-        let connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'jehh8494',
-            database: 'porta_noticias',
-            insecureAuth: true
-        });
-        connection.query('select * from noticia', function(erro, result){
-            res.send(result);
-        });
+        res.render("secao/tecnologia");
 
     });
 };
